f351aa7dd0e4127f6d4300c411180176338f2165
GGoldy <goldypraba@gmail.com>
2025-04-14 22:54:07 +0700

    Vercel File For Deploy

--- database/factories/PenugasanFactory.php
+++ database/factories/PenugasanFactory.php
@@ -22,13 +22,11 @@ class PenugasanFactory extends Factory
     public function definition(): array
     {
         return [
-            'perusahaan'=> $this->faker->randomElement(['SMI']),
+            'perusahaan'=> $this->faker->randomElement(['Toko', 'Kantor']),
             'area'=> $this->faker->randomElement(['Kantor Cabang Surabaya', 'Semarang', 'Samarinda', 'Denpasar', 'Palembang']),
-            'status_karyawan'=> $this->faker->randomElement(['Tetap', 'Kontrak', 'Magang']),
             'unit'=> $this->faker->randomElement(['Departemen A', 'Departemen B', 'Departemen C']),
-            'position' => $this->faker->randomElement(['Posisi A', 'Posisi B', 'Posisi C']),
-            'level' => $this->faker->numberBetween(25, 50),
-            'grade' => $this->faker->sentence(),
+            'level' => $this->faker->numberBetween(1, 8),
+            'grade' => $this->faker->randomElement(['A', 'B', 'C', 'D', 'E']),
             'karyawan_id' => Karyawan::factory(),
             'created_at' => now(),
             'updated_at' => now(),
